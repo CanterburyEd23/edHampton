1f23c16578820c37f70a609b8b50b7487ac22927
CanterburyEd23 <edhampton23@gmail.com>
2022-07-12 11:55:58 +0100

    getWeather data

--- project 1/libraries/js/script.js
+++ project 1/libraries/js/script.js
@@ -1,5 +1,7 @@
 // Step 4: Task - Primary script file
 
+const apiKey = '05b5574976782477ccf8ab24bedc1f41';  //openWeather API
+
 //preloader function
 $(window).on('load', function () {
     if ($('#preloader').length) {
@@ -16,25 +18,43 @@ $(document).ready(function() {
     let countryName;    
     let countryCapital;
     let countryPopulation;
-    let countryHeadOfState;
     let countryCurrency;
-    let countryExchangeRate;
-    let countryWeather ;
+    let countryExchangeRate;    
     let countryWiki;
+    
+    let capitalLat;
+    let capitalLon;
+    
+    let weatherMain;
+    let weatherDetail;
+    let weatherTemp;
+    let weatherHumidity;
+    let weatherWind;
 
-    //setState
+    
+
+    //Update Infobox Function
     function updateInfo() {
         document.getElementById("countryName").innerText = countryName;
         document.getElementById("countryCapital").innerText = countryCapital;
-        document.getElementById("countryPopulation").innerText = countryPopulation;
-        document.getElementById("countryHeadOfState").innerText = countryHeadOfState;
+        document.getElementById("countryPopulation").innerText = countryPopulation;       
         document.getElementById("countryCurrency").innerText = countryCurrency;
-        document.getElementById("countryExchangeRate").innerText = countryExchangeRate;
-        document.getElementById("countryWeather").innerText = countryWeather;
+        document.getElementById("countryExchangeRate").innerText = countryExchangeRate;        
         document.getElementById("countryWiki").innerText = countryWiki;
-    }
+    };    
 
+    //Update Weatherbox Function
+    function updateWeather() {
+        document.getElementById("weatherMain").innerText = weatherMain;
+        document.getElementById("weatherDetail").innerText = weatherDetail;
+        document.getElementById("weatherTemp").innerText = weatherTemp;
+        document.getElementById("weatherHumidity").innerText = weatherHumidity;
+        document.getElementById("weatherWind").innerText = weatherWind;
+    };
+
+    //currently sets all the values to undefined on page load
     updateInfo();
+    updateWeather();
 
 
     //Function that provides the country list and codes to the "Select Country" dropdown menu
@@ -43,7 +63,7 @@ $(document).ready(function() {
             url: "libraries/php/getCountryCodes.php?",
             type: "GET",
             success: function (json) {
-                console.log(json);
+                // console.log(json);
                 let countries = JSON.parse(json);
                 let option = "";
                 for (country of countries) {
@@ -52,7 +72,7 @@ $(document).ready(function() {
                 $("#country_list").append(option);            
             },               
         });
-    }
+    };
 
     get_country_codes();
     
@@ -71,16 +91,42 @@ $(document).ready(function() {
             type: "POST",
             dataType: "json",
             data: { //loading the required parameters into the api call...
-                ISO2Code: iso2, 
+                ISO2Code: iso2
             },
             success: function(result) {
-                console.log(JSON.stringify(result));  //Log the response to console
-                if (result.status.name == "ok") {  //If api returns results as expected, updates State accordingly
+                // console.log(JSON.stringify(result));  //Log the response to console
+                if (result.status.name == "ok") {
                     countryName = result['data'][0]['countryName'];
                     countryCapital = result['data'][0]['capital'];
                     countryPopulation = result['data'][0]['population'];
                     countryCurrency = result['data'][0]['currencyCode'];
+                    getLatLon(countryCapital, apiKey);  //Uses the Capital City name to get co-ordinates, which in turn get the weather for that city
                     updateInfo();  //Updates the Country Info box with all the new information
+                }
+            },
+            error: function(jqXHR, exception) {
+                let msg = "Uncaught Error.\n" + jqXHR.responseText;
+                console.log(msg);
+            }
+        });
+    };    
+
+    //get Capital City Coordinates function
+    function getLatLon(city, apiKey) {
+        $.ajax({
+            url: "libraries/php/getLatLon.php",
+            type: "POST",
+            dataType: "json",
+            data: {
+                CityName: city,
+                APIKey: apiKey
+            },
+            success: function(result) {
+                // console.log(JSON.stringify(result));
+                if (result.status.name == "ok") {  
+                    capitalLat = result['data'][0]['lat'];
+                    capitalLon = result['data'][0]['lon']; 
+                    getWeather(capitalLat, capitalLon, apiKey);  //Uses the acquired coordinates to fetch the weather forecast for the capital
                 }
             },
             error: function(jqXHR, exception) {
@@ -90,7 +136,34 @@ $(document).ready(function() {
         });
     };
 
-    
+    //get Weather in Capital City function
+    function getWeather(lat, lon, apiKey) {
+        $.ajax({
+            url: "libraries/php/getWeather.php",
+            type: "POST",
+            dataType: "json",
+            data: {
+                Lat: lat,
+                Lon: lon,
+                APIKey: apiKey
+            },
+            success: function(result) {
+                // console.log(JSON.stringify(result));
+                if (result.status.name == "ok") {  
+                    weatherMain = result['data']['weather'][0]['main'];
+                    weatherDetail = result['data']['weather'][0]['description'];
+                    weatherTemp = result['data']['main']['temp'];
+                    weatherHumidity = result['data']['main']['humidity'];
+                    weatherWind = result['data']['wind']['speed'];
+                    updateWeather();  //Sets all the weather data in state, then with this function, updates the weatherinfo box
+                }
+            },
+            error: function(jqXHR, exception) {
+                let msg = "Uncaught Error.\n" + jqXHR.responseText;
+                console.log(msg);
+            }
+        });
+    };
 
 
 
---
+++ project 1/libraries/php/getLatLon.php
@@ -0,0 +1,26 @@
+<?php
+    //URL construction
+	$url='http://api.openweathermap.org/geo/1.0/direct?q=' . $_REQUEST['CityName'] . '&limit=1&appid=' . $_REQUEST['APIKey'];
+
+	$ch = curl_init();
+	//cURL object creation
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+	//cURL execution
+    $result=curl_exec($ch);
+	curl_close($ch);  //cURL cleanup
+
+	//Conversion of results into an associative array
+    $decode = json_decode($result,true);	
+
+	//Setting correct header and response info
+    $output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['data'] = $decode;
+	
+	header('Content-Type: application/json; charset=UTF-8');
+
+	echo json_encode($output);
+?>
