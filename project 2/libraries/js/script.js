e9a821da49bd59b20a62c12c9eb070cb02d12590
CanterburyEd23 <edhampton23@gmail.com>
2022-07-26 12:31:04 +0100

    populate select modals

--- project 2/libraries/js/script.js
+++ project 2/libraries/js/script.js
@@ -13,6 +13,8 @@ $(window).on('load', function () {
 //On document ready...
 $(document).ready(function() {
 
+    getAllStaff();
+
     //Get all employees
     function getAllStaff() {
         $.ajax({
@@ -37,6 +39,8 @@ $(document).ready(function() {
                         $("#allStaff").append(listItem);
                     };
                     $("#allStaff").show();
+                    getDepartmentNames();
+                    getSiteNames();
                     $("#staffResultsDetails").show();
                 };
             },
@@ -71,6 +75,7 @@ $(document).ready(function() {
                         $("#allDepartments").append(listItem);
                     };
                     $("#allDepartments").show();
+                    getSiteNames();
                     $("#departmentResultsDetails").show();
                 };
             },
@@ -115,8 +120,81 @@ $(document).ready(function() {
         });
     };
 
-    getAllStaff();
-    
+    //Get department names for select modal
+    function getDepartmentNames() {
+        $.ajax({
+            url: "libraries/php/getAllDepartments.php",
+            type: "GET",
+            success: function(result) {
+                // console.log(JSON.stringify(result));
+                if (result.status.name == "ok") {
+                    let array = result['data'];
+                    populateDepartmentModal(array);
+                };
+            },
+            error: function(jqXHR, exception) {
+                let msg = "Uncaught Error.\n" + jqXHR.responseText;
+                console.log(msg);
+            }
+        });
+    };
+
+    //Get site names for select modal
+    function getSiteNames() {
+        $.ajax({
+            url: "libraries/php/getAllSites.php",
+            type: "GET",
+            success: function(result) {
+                // console.log(JSON.stringify(result));
+                if (result.status.name == "ok") {                   
+                    let array = result['data'];
+                    populateSiteModal(array);
+                };
+            },
+            error: function(jqXHR, exception) {
+                let msg = "Uncaught Error.\n" + jqXHR.responseText;
+                console.log(msg);
+            }
+        });
+    };
+
+    //Other Functions
+    //Populate department select modal
+    function populateDepartmentModal(data) {
+        $("#departmentSelectModalBody").empty();
+        const control = '<div class="form-check">'
+            + '<input class="form-check-input" type="radio" name="flexRadioDefault" id="selectDepartment0" data-bs-dismiss="modal" checked>'
+            + '<label class="form-check-label" for="selectDepartment0">all Departments</label>'
+        + '</div>';
+        $("#departmentSelectModalBody").append(control);
+        let department;
+        for (let i = 0; i < data.length; i++) {
+            department = '<div class="form-check">'
+            + '<input class="form-check-input" type="radio" name="flexRadioDefault" id="selectDepartment' + data[i]["id"] + '" data-bs-dismiss="modal">'
+            + '<label class="form-check-label" for="selectDepartment' + data[i]["id"] + '">' + data[i]["name"] + '</label>'
+        + '</div>';
+        $("#departmentSelectModalBody").append(department);
+        };
+    };
+
+    //Populate site select modal
+    function populateSiteModal(data) {
+        $("#siteSelectModalBody").empty();
+        const control = '<div class="form-check">'
+            + '<input class="form-check-input" type="radio" name="flexRadioDefault" id="selectSite0" data-bs-dismiss="modal" checked>'
+            + '<label class="form-check-label" for="selectSite0">all Sites</label>'
+        + '</div>';
+        $("#siteSelectModalBody").append(control);
+        let site;
+        for (let i = 0; i < data.length; i++) {
+            site = '<div class="form-check">'
+            + '<input class="form-check-input" type="radio" name="flexRadioDefault" id="selectSite' + data[i]["id"] + '" data-bs-dismiss="modal">'
+            + '<label class="form-check-label" for="selectSite' + data[i]["id"] + '">' + data[i]["name"] + '</label>'
+        + '</div>';
+        $("#siteSelectModalBody").append(site);
+        };
+    };
+
     //Full list select buttons
     $("#allStaffButton").click(function(){
         getAllStaff();        
